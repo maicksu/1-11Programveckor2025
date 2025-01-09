7ccc1c950817871a7427ad12331270b78870f5d7
Rex-drive <rex.mansson@elev.ga.lbs.se>
2025-01-09 13:59:55 +0100

    move typ

---
+++ Programveckor/Assets/movement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public int accelerationSpeed;
+    public int maxSpeed;
+    public int friction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-5, 0);
+            // walk left
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(5, 0);
+            // walk right
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * 1000);
+        }
+    }
+}
