5b38c5b0387f771de17ad0d8fe4c1e754f1cf94d
Jhnrfael <john.mendozatovera@elev.ga.lbs.se>
2025-01-14 09:13:49 +0100

    Main Menu, Pause Menu

---
+++ Programveckor/Assets/PauseManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pauseMenuUI; // Assign the Pause Menu Panel in the Inspector
+
+    private bool isPaused = false;
+
+    void Update()
+    {
+        // Check if the Escape key is pressed
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false); // Hide the pause menu
+        Time.timeScale = 1f;         // Resume game time
+        isPaused = false;
+    }
+
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true); // Show the pause menu
+        Time.timeScale = 0f;         // Freeze game time
+        isPaused = true;
+    }
+}
